a5b616448d03f18e9f5af9e8b6540eab63d6d0f3
Yahel Tubul <yaeltobol07@gmail.com>
2025-12-24 12:30:57 +0200

    add function split_tokenize to define each char in the expression the type, complete the part of number

--- Calculator.py
+++ Calculator.py
@@ -14,18 +14,19 @@ class Token:
     def __init__(self, type, value):
         self.type = type
         self.value = value
-    #pass each char in the exprssion
+    #pass each char in the exprssion and defines each character in the expression to the type of the token.
     def split_tokenize(expression : str) -> list:
         tokens : list = []
         long_num : str = ""
         for char in expression:
+            #check if the char is number or float number and for number with two numbers and more
             if (char.isdigit() or (char == '.' and '.' not in long_num)):
                 long_num += char
             else:
+                #add the number to the tokens list
                 if long_num:
                     tokens.append(Token(Tokenize.NUMBER, long_num))
                     long_num = ""
-
 
 
 
