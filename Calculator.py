0eb8adaf6475853393b789e9a6f2b9d7fcfb443c
Yahel Tubul <yaeltobol07@gmail.com>
2025-12-24 12:24:24 +0200

    add function split_tokenize to define each char in the expression the type

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,31 @@
 from enum import Enum
-#create enum for definition the types of token
+
+
+# create enum for definition the types of token
 
 class Tokenize(Enum):
     NUMBER = 1
     OPERATOR = 2
     LPAREN = 3
     RPAREN = 4
+
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+    #pass each char in the exprssion
+    def split_tokenize(expression : str) -> list:
+        tokens : list = []
+        long_num : str = ""
+        for char in expression:
+            if (char.isdigit() or (char == '.' and '.' not in long_num)):
+                long_num += char
+            else:
+                if long_num:
+                    tokens.append(Token(Tokenize.NUMBER, long_num))
+                    long_num = ""
+
+
+
+
