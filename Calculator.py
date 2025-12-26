09121a7d04d02419aed082557314fa60a484ce29
Yahel Tubul <yaeltobol07@gmail.com>
2025-12-26 22:49:43 +0200

    add reference to check negative numbers

--- Calculator.py
+++ Calculator.py
@@ -9,41 +9,44 @@ class Tokenize(Enum):
     LPAREN = 3
     RPAREN = 4
 
-operators : char = ['+','-','*','/','^','%','$','&','@','~','!']
+
+operators: char = ['+', '-', '*', '/', '^', '%', '$', '&', '@', '~', '!']
+
 
 class Token:
     def __init__(self, type, value):
         self.type = type
         self.value = value
-    #pass each char in the exprssion and defines each character in the expression to the type of the token.
-    def split_tokenize(expression : str) -> list:
-        tokens : list = []
-        long_num : str = ""
+
+    # pass each char in the exprssion and defines each character in the expression to the type of the token.
+    def split_tokenize(expression: str) -> list:
+        tokens: list = []
+        long_num: str = ""
+        token_type: Tokenize = None
         for char in expression:
-            #check if the char is number or float number and for number with two numbers and more
-            if (char.isdigit() or (char == '.' and '.' not in long_num)):
+            #check for negative numbers. by comare if the prev token its not a number type
+            if char == '-' and (prev_token_type in [None, Tokenize.OPERATOR, Tokenize.LPAREN]):
+                long_num += char
+            # check if the char is number or float number and for number with two numbers and more
+            elif (char.isdigit() or (char == '.' and '.' not in long_num)):
                 long_num += char
             else:
-                #add the number to the tokens list
+                # add the number to the tokens list
                 if long_num:
                     tokens.append(Token(Tokenize.NUMBER, long_num))
                     long_num = ""
-                #check if char is a valid operator,and add it to list
+                # check if char is a valid operator,and add it to list
                 if (char in operators):
                     tokens.append(Token(Tokenize.OPERATOR, char))
-                #check if char is left paren
+                # check if char is left paren
                 elif (char == '('):
                     tokens.append(Token(Tokenize.LPAREN, char))
-                #check if char is right paren
+                # check if char is right paren
                 elif (char == ')'):
                     tokens.append(Token(Tokenize.RPAREN, char))
                 else:
                     raise ValueError("Invalid token")
-        #add if there is the last char to the list
+        # add if there is the last char to the list
         if long_num:
             tokens.append(Token(Tokenize.NUMBER, long_num))
         return tokens
-
-
-
-
